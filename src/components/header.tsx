ac6a4748f219f2e015619cd3185a6ab4153ca306
zadoke <rafaelajs@pm.me>
2023-07-20 00:53:35 +0100

    refactor(Header): remove unused import

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faTrain } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 interface HeaderProps {
     type: 'station' | 'train' | 'none';
