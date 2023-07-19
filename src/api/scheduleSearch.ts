288714d044efa53b6aba5feb110ec0bb427bbf09
Tiago Calado <1tiagocalado8@gmail.com>
2023-07-19 23:15:58 +0100

    Fix Tailwind dependencies and Endpoint port

--- src/api/scheduleSearch.ts
+++ src/api/scheduleSearch.ts
@@ -1,12 +1,11 @@
 export async function scheduleSearch(stationId: number) {
     const api = `http://127.0.0.1:8000/station/${stationId}/departures`;
-  
+
     try {
-      const response = await fetch(api);
-      const data = await response.json();
-      return data;
+        const response = await fetch(api);
+        const data = await response.json();
+        return data;
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  }
-  
+}
--- src/api/stationSearch.ts
+++ src/api/stationSearch.ts
@@ -1,15 +1,17 @@
 export async function stationSearch(searchQuery: string) {
-    try{
-        const response = await fetch(`http://127.0.0.1:8000/station/search/${searchQuery}`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json'
+    try {
+        const response = await fetch(
+            `http://127.0.0.1:8000/station/search/${searchQuery}`,
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                },
             }
-        });
+        );
         const data = await response.json();
         return data;
-    } catch (error){
+    } catch (error) {
         console.error(error);
     }
-  }
-  
+}
